6ce3579250ccc6ef9293b2c14896a76718573bf6
Renan Braulio <renanbraulio@MacBook-Pro-de-Renan.local>
2020-11-12 13:27:32 -0300

    projeto angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lista-pessoa/lista-pessoa.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaPessoaComponent } from './lista-pessoa.component';
+
+describe('ListaPessoaComponent', () => {
+  let component: ListaPessoaComponent;
+  let fixture: ComponentFixture<ListaPessoaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaPessoaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaPessoaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
